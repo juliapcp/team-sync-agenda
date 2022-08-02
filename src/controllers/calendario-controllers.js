30556ffbe433338cadb08818367e1e2f0b687388
juliapcp <juliapcp2003@gmail.com>
2022-08-02 11:11:23 -0300

    Finalização do calendário

--- src/controllers/calendario-controllers.js
+++ src/controllers/calendario-controllers.js
@@ -1,8 +1,6 @@
 
 const { Usuario } = require('../models/Usuario');
 const { Reuniao } = require('../models/Reuniao');
-const { UsuarioTime } = require('../models/UsuarioTime');
-const { Time } = require('../models/Time');
 
 class CalendarioController {
 
@@ -10,18 +8,9 @@ class CalendarioController {
         if (req.params.data){
             if(req.params.idUsuario){
                 const usuarios = await Usuario.findAll();
-                const reunioes = await Reuniao.findAll({
-                    where: {
-                        "usuarioId": req.params.idUsuario
-                    },
-                    include: [
-                        {
-                            model: Time, as: 'time',
-                            required: false
-                        }
-                    ]
-                })
-                return res.render('calendario/index', { data: req.params.data.concat('T00:00'), usuarios, idUsuario: req.params.idUsuario });
+                const ReuniaoModel = new Reuniao();
+                const reunioes = await ReuniaoModel.findByUsuario(req.params.idUsuario, req.params.data);
+                return res.render('calendario/index', { data: req.params.data.concat('T00:00'), usuarios, idUsuario: req.params.idUsuario, reunioes });
             } else {
                 return res.redirect('/calendario/' + req.params.data +'/'+req.session.usuario.id);
             }
--- src/models/Reuniao.js
+++ src/models/Reuniao.js
@@ -2,17 +2,37 @@ const { DataTypes, Model } = require('sequelize');
 
 const { sequelizeCon } = require('../config/db-config');
 
-class Reuniao extends Model { }
+class Reuniao extends Model { 
+    async findByUsuario(idUsuario, data) {
+        const dataFinal = new Date(data + 'T00:00');
+        dataFinal.setDate(dataFinal.getDate()+1);
+        const [results] = await sequelizeCon.query(`
+                select 
+                    reuniaos.descricao as "descReuniao", times.descricao as "descTime", "horaReuniao" 
+                from
+                    reuniaos
+                left join usuariotimes on
+                    reuniaos."timeId" = usuariotimes."timeId"
+                left join times on 
+                	reuniaos."timeId" = times.id    
+                where
+                    usuariotimes."usuarioId" = ${ idUsuario } 
+                    and reuniaos."dataReuniao" >= '${data}'
+                    and reuniaos."dataReuniao" < '${dataFinal.toISOString().substring(0,10)}'`);
+        return results;
+    }
+}
 
 Reuniao.init({
-    datahora: DataTypes.DATE,
+    dataReuniao: DataTypes.DATE,
+    horaReuniao: DataTypes.TIME,
+    descricao: DataTypes.STRING
 }, {
     sequelize: sequelizeCon,
     schema: 'public',
     modelName: 'reuniao',
     createdAt: false,
-    updatedAt: false,
-    timestamps: true
+    updatedAt: false
 }, true);
 
 module.exports = { Reuniao };
